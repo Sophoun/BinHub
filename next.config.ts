11f6806ba39b0404a8bf8edf27a49b2b0c2ab332
Nheum Sophoun <nheumsophoun@CMCBs-MacBook-Pro.local>
2026-05-28 05:07:44 +0700

    add baspath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: "/binhub",
   allowedDevOrigins: ["http://localhost:3000"],
   experimental: {
     serverActions: {
